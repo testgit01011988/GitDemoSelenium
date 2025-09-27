7ca826aa1b2ed8fded0a78ec7fa3de3714c6509a
UserA <test@gmail.com>
2025-09-27 20:56:16 +0530

    Committed by user B

--- src/ExampleOfUserA.java
+++ src/ExampleOfUserA.java
@@ -4,6 +4,10 @@ public class ExampleOfUserA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First line printed by User A");
+		
+		System.out.println("First update made by User B after cloning code from GITHUB");
+		
+		
 	}
 
 }
